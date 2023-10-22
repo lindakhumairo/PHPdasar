4719ebb3816c96c83418b75c456b7565681c0cdc
unknown <layalilinda4@gmail.com>
2023-10-22 11:07:12 +0700

    latihan

---
+++ Tugas/latihan.php
@@ -0,0 +1,89 @@
+<?php
+    echo "Halo"; echo"<br>";
+    //penamaan variabel
+      $andi="Linda";
+       echo $andi; echo "<br>";
+
+    //menampilkan nilai variabel
+    echo "MENAMPILKAN NILAI VARIABEL";
+    echo "<br>";
+    $a=5;
+    $b="Saya Belajar PHP $a";
+    echo $b; echo "<br>";
+
+    //tipe data integer
+    echo "TIPE DATA INTEGER 1";
+    echo "<br>";
+     $umur=21;
+     $harga=15000;
+     $rugi=-500000;
+
+    echo $umur;
+    echo "<br>";
+    echo $harga;
+    echo "<br>";
+    echo $rugi;
+    echo "<br>";
+
+    $c=14;
+    $d=16;
+    $e=$c + $d;
+    echo $e;
+    echo "<br>";
+
+    $f=$c * $d;
+    echo $f;
+    echo "<br>";
+
+    echo "TIPE DATA FLOAT";
+    echo "<br>";
+    $float1=10.66;
+    $float2=12.4;
+    $floathasil= $float1 + $float2;
+
+    $floatbagi= $float1 / $float2;
+    echo $floathasil;
+    echo "<br>";
+    echo $floatbagi;
+    echo "<br>";
+
+    echo "TIPE DATA STRING";
+    echo "<br>";
+    $string1='Ini adalah String sederhana';
+    $string2="Ini adalah String
+    yang bisa memiliki beberapa baris";
+    $string3='Dia berkata : "I \'ll be back"';
+    echo $string1; echo "<br>";
+    echo $string2; echo "<br>";
+    echo $string3; echo "<br>";
+
+    echo "TIPE DATA BOLEAN";
+    echo "<br>";
+    $benar=true;
+    $salah=false;
+
+    echo "benar = $benar, salah = $salah";
+    echo "<br>";
+
+    echo "OPERTAOR ARITMATIKA";
+    echo "<br>";
+    $hasil1= -3;
+    $hasil2=3+5;
+    $hasil3= 8-2.4;
+    $hasil4= 2*5;
+    $hasil5= 3+8/5-3;
+    $hasil6= 10 % 4;
+    echo "$hasil1:"; var_dump($hasil1);
+    echo "<br \>";
+    echo "$hasil2:"; var_dump($hasil2);
+    echo "<br \>";
+    echo "$hasil3:"; var_dump($hasil3);
+    echo "<br \>";
+    echo "$hasil4:"; var_dump($hasil4);
+    echo "<br \>";
+    echo "$hasil5:"; var_dump($hasil5);
+    echo "<br \>";
+    echo "$hasil6:"; var_dump($hasil6);
+    echo "<br \>";
+
+    ?>
